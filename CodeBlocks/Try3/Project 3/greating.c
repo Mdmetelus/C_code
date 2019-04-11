04326d1ba3c4b3367f2c6af094b6f6ad83910a1f
Mr. Metelus <Mdmetelus@gmail.com>
2019-04-11 17:15:01 -0400

    comment...

--- CodeBlocks/Try3/Project 3/greating.c
+++ CodeBlocks/Try3/Project 3/greating.c
@@ -3,4 +3,11 @@ int main()
 {
     printf("Hi My name is.... Mr.Metelus");
     return 0;
+    // single line comments
+
+    /*multiline comments
+        -multiline comments
+        -multiline comments
+        -multiline comments
+            multiline comments*/
 }
