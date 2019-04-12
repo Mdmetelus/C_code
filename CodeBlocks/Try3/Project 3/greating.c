e9bdf2d28937382cd8da39810d7b0b85da68b38f
Mr. Metelus <Mdmetelus@gmail.com>
2019-04-12 02:59:53 -0400

    update

--- CodeBlocks/Try3/Project 3/greating.c
+++ CodeBlocks/Try3/Project 3/greating.c
@@ -1,4 +1,10 @@
+/*
+- preprocessor statements are begin with the pound sign.
+- includes is one type of preprocessor statement "directive".
+- includes is like an import statment.
+************************************************ */
 #include <stdio.h>
+
 int main()
 {
     printf("Hi My name is.... Mr.Metelus");
