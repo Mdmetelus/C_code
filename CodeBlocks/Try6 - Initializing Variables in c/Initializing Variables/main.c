c0b6f6d05900361050b66acd62d2ad2b3e8dc957
Mr. Metelus <Mdmetelus@gmail.com>
2019-04-13 18:27:20 -0400

    updated

---
+++ CodeBlocks/Try6 - Initializing Variables in c/Initializing Variables/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //printf("Hello world!\n");
+    //return 0;
+    int jason = 5;
+    jason = 8;
+    float tom = 23.3334;
+    float cash =  19.95;
+
+    double biggie = 55.55555555555555555555555e+11;
+
+    bool boolVariable = true;
+
+    bool ToBe = 1;
+
+    bool notToBe = false;
+
+    short int shorty = 11;
+
+    long int stuff = 99999999999999999L;
+
+    long double US_Deficit_2071 = 1.234e+7L;
+
+
+    signed short
+    unsigned //nonnegative
+    unsigned int  //nonnegative numbers
+
+    enum myColor {white, gray, black};
+
+    enum month {January, Febuary, March, April, May, June, July, August, September, October, November, December};
+    //enum means the variable month can only be one of the months listed in the brackets. Nothing else can be set to that value.
+
+    enum direction {up, down, left =10, right}; // up === 0, down === 1, left === 10, right === 11;
+
+
+    char more;
+    //anything inside a single quote ; letter character or semicolon
+
+    char broiled;
+    broiled = 'T';
+
+    char x = '\n';
+}
