f33a1231715784216c9efbf12aad3ef2c4b374b0
Mr. Metelus <Mdmetelus@gmail.com>
2019-04-14 01:03:26 -0400

    ...

---
+++ basics2/hello/usignedOne.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+  unsigned char x = 12; // an unsigned is a non-negative int between 0 and 255;
+  
+  printf("%u\n", x);
+
+  /*
+  int a[3] = {10, 20, 30};
+
+  int i = 2;
+
+  printf("%d %d\n", i, a[i]);
+
+  int result;
+
+  result = add(12, 3);
+
+  printf("%d\n", result);
+  */
+
+  return 0;
+}
