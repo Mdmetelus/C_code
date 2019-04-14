b9daa82d7733202744d5ececa498bdcae40b50a6
Mr. Metelus <Mdmetelus@gmail.com>
2019-04-14 00:47:13 -0400

    array made

---
+++ basics2/array/array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/*
+int add(int x, int y)
+{
+  return x + y;
+}
+*/
+int main(void) {
+
+  int a[3] = { 10, 20, 30};
+
+  int i =2;
+
+  printf("%d %d\n", i, a[i]);
+
+  /*
+  int result;
+
+  result = add(12, 3);
+
+  printf("%d\n", result);
+  */
+
+  return 0;
+}
