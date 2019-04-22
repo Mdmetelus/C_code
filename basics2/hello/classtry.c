878f6bd2fe899e1232b56dab030da68454c4b65b
Mr. Metelus <Mdmetelus@gmail.com>
2019-04-22 12:45:36 -0400

    ...

---
+++ basics2/hello/classtry.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+  int x = 2;
+  float y = 3.14;
+
+  printf("x is %d\n", x);
+  printf("y is %f\n", y);
+
+  int z;
+
+  z = 3 + x; 
+
+  float f;
+
+  f = 3 + 2.6;
+
+  return 0;
+}
