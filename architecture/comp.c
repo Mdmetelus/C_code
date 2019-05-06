9f6be69ca946674169fed9ed47426a8717372426
Mr. Metelus <Mdmetelus@gmail.com>
2019-05-06 13:35:52 -0400

    ...

--- architecture/comp.c
+++ architecture/comp.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define PRINT_BEEJ
-#define HALT
+#define PRINT_BEEJ 1
+#define HALT 2
 
 // char holds i 8 bit value
 // largest value is 255 smallest value is zero
@@ -14,5 +15,22 @@ unsigned char memory[5] = {
   };
 
 int main(void) {
+  int halted = 0;
+  int pc = 0; // Program Counter, index of the current instruction in
+
+  while(!halted) {
+    int current_inst = memory[pc];
+    switch(current_inst) {
+        case PRINT_BEEJ:
+            print("Beej!\n");
+            break;
+
+        default:
+            printf("Unknown")
+    }
+
+    pc++; // move to next instruction
+
+  }
 
 }
