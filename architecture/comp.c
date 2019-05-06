4a96b83f4e63090f09d88ea6c783dac4e38d00ca
Mr. Metelus <Mdmetelus@gmail.com>
2019-05-06 13:28:56 -0400

    update

---
+++ architecture/comp.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define PRINT_BEEJ
+#define HALT
+
+// char holds i 8 bit value
+// largest value is 255 smallest value is zero
+unsigned char memory[5] = {
+    PRINT_BEEJ,
+    PRINT_BEEJ,
+    PRINT_BEEJ,
+    PRINT_BEEJ,
+    HALT
+  };
+
+int main(void) {
+
+}
